fa9df9e628b1b56bd41bfb8abe5ee698220915e3
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-05-19 13:55:49 +0100

    Implemented listener and display features

--- src/weather_app.js
+++ src/weather_app.js
@@ -15,9 +15,37 @@ let getTemperature = city => {
     .then(function(response) {
       return response.json();
     })
-    .then(function(json) {})
-    .catch(function(err) {});
+    .then(function(json) {
+      displayTemp(json);
+    })
+    .catch(function(err) {
+      displayErr(msg);
+    });
 };
+
+let displayTemp = json => {
+  const celcius = json.main.temp;
+  getFehDiv().innerHTML = toFahrenheit(celcius);
+  getCelDiv().innerHTML = celcius;
+};
+
+let displayErr = msg => {
+  getErrDiv().innerHTML = msg;
+  clearInput();
+  setTimeout(clearErr, 2000);
+};
+
+const isValid = city => {
+  return city === "" ? false : true;
+};
+
+getSearchBtn().addEventListener("click", e => {
+  e.preventDefault();
+  getCelDiv().innerHTML = "";
+  getFehDiv().innerHTML = "";
+  const city = getInput().value;
+  if (isValid(city)) getTemperature(city);
+});
 
 let urlBuilder = (k, c) => {
   return `https://api.openweathermap.org/data/2.5/weather?q=${c}&appid=${k}&units=metric`;
